63d05c23a1a4b92ecd0413b43ae497f8bc99c9e7
Tim0nka <timonvaismann@tutamail.com>
2024-10-18 11:38:05 +0300

    Готово

--- simple.py
+++ simple.py
@@ -1,15 +1,9 @@
-input_data = open('input.txt', 'r')
-output_data = open ('output.txt','a')
-output_data = open ('output.txt','w')
-data = input_data.read()
-a = int(data)
+a = int(input("Введите число: "))
 k = 0
 for i in range(2, a // 2+1):
     if (a % i == 0):
         k = k+1
 if (k <= 0):
-    output_data.write("Число простое")
+    print("Число простое")
 else:
-    output_data.write("Число не является простым")
-input_data.close()
-output_data.close()
+    print("Число не является простым")
